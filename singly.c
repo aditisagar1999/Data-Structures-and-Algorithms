f370c4d7812af76a5f0bf9fe9d335606f2462962
aditi <aditisagar19@gmail.com>
2024-11-04 12:04:09 -0500

    New changes

--- singly.c
+++ singly.c
@@ -1,3 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int number;
+    struct node *next;
+}node;
+
+node *n = NULL;
+
+void insert_node(int x);
+
+
+int main(void)
+{
+    int i = 2;
+
+
+    while (i < 12)
+    {
+        insert_node(i);
+        i += 2;
+    }
+
+    node *ptr = n;
+    while(ptr)
+    {
+     printf("%d ", ptr->number);
+     ptr = ptr->next;
+    }
+    printf("\n");
+
+}
+
+void insert_node(int x)
+{
+    node *temp = malloc(sizeof(node));
+    temp->number = x;
+    temp->next = n;
+
+    n = temp;
+
+}
